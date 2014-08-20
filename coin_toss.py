06f4a9d9c07b34198f807998f261edb17f7479ef
JL-Msi Ubuntu <world.jadi@gmail.com>
2014-08-20 23:02:22 +0700

    cal number trial

--- coin_toss.py
+++ coin_toss.py
@@ -1,11 +1,25 @@
 import random
  
-N = 100
+N = 10
 c = 0
-for i in range(N):
-    head = random.random() >= 0.5
-    if head:
-        c += 1
- 
-print "Heads:",c
-print "Freq/N:",float(c)/N
+
+check = []
+
+for i in range(N+1):
+	check.append(0.0)
+
+for j in range(N):
+	for i in range(N):
+	    head = random.random() >= 0.5
+	    if head:
+	        c += 1
+	check[c] += 1
+	c = 0
+
+for i in range(N+1):
+	print check[i]/N
+
+print '-------------------------------'
+print sum(check)
+# print "Heads:",c
+# print "Freq/N:",float(c)/N
