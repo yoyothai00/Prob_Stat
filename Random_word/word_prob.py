9d937c97f9b1b4a8427a392ee17d2d691c43bce6
JL-Msi Ubuntu <world.jadi@gmail.com>
2014-08-23 19:33:59 +0700

    find a & b

--- Random_word/word_prob.py
+++ Random_word/word_prob.py
@@ -2,23 +2,23 @@ text_file = open("word.txt", "r")
 line = text_file.readlines()
 
 #alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' , 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 # check = line[0].split('\n')[0]
+
 count = 0
-
-alph = raw_input('alphabet: ')
-
+# alph = raw_input('alphabet: ')
 for i in range(len(line)):
-	word = line[i].split()
-	check = line[i].split(alph)
-	if len(word) != len(check):
-		count += 1
+	word = list(line[i])
+	temp_count = 0
+	for j in range(len(word)):
+		if 'a' == word[j]:
+			temp_count += 1
+		
+	if temp_count >= 1:
+		count +=1
+
 
 prob = float(count)/len(line)
 print 'prob:',prob
-
 # prob = float(count/len(text))
-
 # print prob
-
 text_file.close()
