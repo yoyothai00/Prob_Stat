b4bed3c150c79cb29dc55c3390f53302795daee9
JL-Msi Ubuntu <world.jadi@gmail.com>
2014-08-26 13:24:05 +0700

    more than 3 vowels

--- Random_word/word_prob.py
+++ Random_word/word_prob.py
@@ -1,19 +1,23 @@
 text_file = open("word.txt", "r")
 line = text_file.readlines()
 
-#alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' , 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# check = line[0].split('\n')[0]
-
 count = 0
 # alph = raw_input('alphabet: ')
+vowel = ['a', 'e', 'i', 'o', 'u']
+
 for i in range(len(line)):
 	word = list(line[i])
+	check_vowel = False
+	temp_count = 0
 	for j in range(len(word)):
-		if 'a' == word[j] or 'b' == word[j]:
-			count += 1
+		for k in range(len(vowel)):
+			if word[j] == vowel[k]:
+				temp_count += 1
+				break
+	if temp_count > 3:
+		count+=1
 
 prob = float(count)/len(line)
 print 'prob:',prob
-# prob = float(count/len(text))
-# print prob
+
 text_file.close()
